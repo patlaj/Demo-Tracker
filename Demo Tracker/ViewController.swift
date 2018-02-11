7dcf8be09f2a22a89e4afdeb6fa538fb0a091fba
Patlaj <patlaj@gmail.com>
2018-02-11 11:52:52 -0500

    Initial Commit

---
+++ Demo Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo Tracker
+//
+//  Created by Patrick Lajeunesse on 2018-02-11.
+//  Copyright © 2018 Patrick Lajeunesse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
